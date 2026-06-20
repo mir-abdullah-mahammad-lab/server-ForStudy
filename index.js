99891813371722c5cfc588b1e6abb2ea9f483a2a
mir-abdullah-mahammad-lab <mirabdullahmahammad@gmail.com>
2026-06-20 11:56:33 +0600

    ready-for-vercel

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(process.env.MONGODB_URI, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const db = client.db('quiet-study')
     const allRooms = db.collection('all-rooms')
     const timeSlot = db.collection('time-slot-booking')
@@ -79,8 +79,8 @@ async function run() {
     })
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
